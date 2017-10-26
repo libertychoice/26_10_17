0e095bdd1f02a8cf714e7953ead74ff705b93417
Student <test@test.com>
2017-10-26 22:24:54 +0300

    add files

--- index.js
+++ index.js
@@ -9,5 +9,30 @@ module.exports = {
     test_3: 3
 }
 
+//console.log(os);
 
-module.exports = () => {}
+const express = require("express");
+const app = express();
+app.get("/",(req,res,next)=>{
+    res.status(200);
+    res.setHeader("Content-type","text/html")
+    //res.setHeader("Content-type","plain/text") //try to save text
+    res.send(
+        `<h1>TEEEST</h1>`
+    )
+   // res.json({
+   //     hello:"world"
+   // })
+})
+//app.all
+//app.get
+//app.post
+//app.put
+//app.delete
+
+app.listen(3000,()=>{
+    console.log("Server starts on 3000 port");
+})
+//app.listen("0.0.0.0:3000")
+
+//module.exports = () => {}
